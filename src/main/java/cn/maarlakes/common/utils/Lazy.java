8a2cf18760e70388b40b410527fc9254f54b200d
linjpxc <815488987@qq.com>
2024-01-17 09:33:14 +0800

    调整Lazy

--- src/main/java/cn/maarlakes/common/utils/Lazy.java
+++ src/main/java/cn/maarlakes/common/utils/Lazy.java
@@ -12,27 +12,10 @@ public interface Lazy<T> extends Supplier<T> {
     boolean isCreated();
 
     @Nonnull
-//    static <T> Lazy<T> of(@Nonnull Supplier<T> factory) {
-//        return new DefaultLazy<>(factory);
-//    }
-
-    static <T> Supplier<T> of(@Nonnull Supplier<T> factory) {
-        return new Supplier<T>() {
-
-            private volatile T value = null;
-            private final Object sync = new Object();
-
-            @Override
-            public T get() {
-                if (this.value == null) {
-                    synchronized (this.sync) {
-                        if (this.value == null) {
-                            this.value = factory.get();
-                        }
-                    }
-                }
-                return this.value;
-            }
-        };
+    static <T> Lazy<T> of(@Nonnull Supplier<T> factory) {
+        if (factory instanceof Lazy) {
+            return (Lazy<T>) factory;
+        }
+        return new DefaultLazy<>(factory);
     }
 }
